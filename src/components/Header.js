d6dd994d5995c62390d5bd93b1cbe4719e1e2ac9
Markeljan <Markeljan19@gmail.com>
2022-07-18 16:09:55 -0400

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+import troll from "../troll.svg";
+
+function Header() {
+    return(
+        <div className="header-container">
+            <div>
+                <img src={troll} className="header-img" alt="troll face" />
+                <p>Meme Generator</p>          
+            </div>
+            <p className="header-text">HackFS - Meme Generator</p>
+        </div>
+        )
+}
+
+export default Header;
+        
